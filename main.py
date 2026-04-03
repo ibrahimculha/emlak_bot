6b10f35d77eceef6161b9d799eadc29779c15a2e
ibrahimculha <ibrahimculha1996@gmail.com>
2026-04-03 04:57:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 TOKEN = "8761053450:AAEtCKoJdW5bgv5U7mVtKR_MdYzmcWaD-nM"
 CHAT_ID = "6680927334"
 
-bot = Bot(token="8761053450:AAEtCKoJdW5bgv5U7mVtKR_MdYzmcWaD-nM")
+bot = Bot(token=TOKEN)
 
 try:
     with open("db.json", "r") as f:
